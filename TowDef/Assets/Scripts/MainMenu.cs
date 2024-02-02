4e0010e889c0f841e84449b737c932174de46b5e
leoricca02 <leo.ricca@stud.uniroma3.it>
2024-02-02 13:23:01 +0100

    FADING
    Aggiunta animazione fading da MainMenu a MainLevel e viceversa

--- TowDef/Assets/Scripts/MainMenu.cs
+++ TowDef/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
    public string levelToLoad = "MainLevel";
+
+    public SceneFader sceneFader;
+
     public void Play ()
     {
-        SceneManager.LoadScene(levelToLoad);
+        sceneFader .FadeTo(levelToLoad);
     }
 
     public void Quit()

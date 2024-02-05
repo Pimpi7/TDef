1d2fd642bc192d38302a81eda4de579f9a96ca52
sfanta <37906186+sfanta@users.noreply.github.com>
2024-02-05 21:46:28 +0100

    Aggiunta HealthBar
    Aggiunta HelathBar e canvas per aggiungere info in sovraimpressione sul nemico

--- TowDef/Assets/Scripts/Enemy.cs
+++ TowDef/Assets/Scripts/Enemy.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
-
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
     [HideInInspector]
     public float speed;
-    public float health = 100;
+    public float startHealth = 100;
+    private float health;
     public int earnedMoney = 50;
     public GameObject deathEffect;
     
+    [Header("Unity stuff")]
+    public Image HealthBar;
     void Start() {
 
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+        HealthBar.fillAmount = health / startHealth;
         if (health <= 0)
         {
             Die();
